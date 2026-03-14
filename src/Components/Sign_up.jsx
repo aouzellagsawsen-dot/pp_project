4244ffbe26ef1509f024d483f8c645edb3f5907c
Mimi986 <chelouah.mounia@gmail.com>
2026-03-14 22:29:57 +0100

    feat(ajout dîcones et ajustements du bouton, police,etc)

--- src/Components/Sign_up.jsx
+++ src/Components/Sign_up.jsx
@@ -1,56 +1,69 @@
 import React from 'react'
 import { BookOpen } from 'lucide-react';
 import { Star } from 'lucide-react';
+import {House} from 'lucide-react'
+import { LayoutGrid } from 'lucide-react';
 import {Sparkle} from 'lucide-react'
 
 const Sign_up = () => {
   return (
     <div className='flex justify-between flex-col items-center min-h-screen bg-[#F1EAD7] gap-[2]'>
       
-      <div className='flex items-center mt-16'>
-      <BookOpen strokeWidth={2.5} size={32} className='text-[#5C544B]'> </BookOpen>
-      <h1 className='text-[#8D7B68] text-center text-[30px] font-[Lora]'>Alinéa</h1>
+      <div className='flex items-center mt-16 gap-2 mb-6'>
+      <BookOpen strokeWidth={2.5} size={32} className='text-[#8D7B68]'></BookOpen>
+      <h1 className='text-[#5c544b] text-center text-[30px] font-serif'>Alinéa</h1>
       </div>
-      <form className='my-auto flex font-[Lora] flex-col bg-[#FAF6F0] pb-20 max-w-[500] min-w-[450] max-h-[700] min-h-[650] px-6 py-12 rounded-[20px] font-extrabold gap-2.5'>
+      
+      <form className='my-auto flex font-[Lora] flex-col bg-white/60 pb-20 max-w-[500] min-w-[450] max-h-[700] min-h-[650] px-6 py-12 rounded-[20px] font-extrabold gap-2.5'>
+        
+        <div className="flex items-center justify-center gap-4 mb-6">
+          <div className="h-px w-16 bg-linear-to-r from-transparent to-[#8D7B68]"></div>
+          <LayoutGrid size={14} strokeWidth={4} className="text-[#8D7B68]" />
+          <div className="h-px w-16 bg-linear-to-r from-transparent to-[#8D7B68]"></div>
+        </div>
+
         <h1 className='text-[#5C544B] text-4xl font-serif font-semibold text-[32px] tracking-tight text-center'>Join Our Community</h1>
-        <p className='text-[#7A6A5A] text-[16px] font-medium text-center'>- Start sharing and discovering amazing books -</p>
+        <p className='text-[#7A6A5A] text-[16px] font-medium text-center italic'>- Start sharing and discovering amazing books -</p>
 
         <div>
-        <label className='font-[Inter] text-[#7A6A5A] size-[14] font-medium' htmlFor="name" value="name">Full Name</label>
-        <input className='bg-[#ffffff] rounded-xl w-full' type="text" placeholder="" id="name"></input>
-        </div>
-
-        <div>
-        <label className='font-[Inter] text-[#7A6A5A] font-medium' htmlFor="email" value="email">Email</label>
-        <input className='bg-[#ffffff] rounded-xl p-[50] w-full' type="email" placeholder="" id="email"/>
+        <label className='font-sans text-[#7A6A5A] size-[14] font-medium' htmlFor="name" value="name" required>Full Name</label>
+        <input className='placeholder:text-[#e6cbb2] placeholder:font-extralight placeholder:font-sans focus:ring-1 border border-[#EFE7D6] focus:outline-none text-[#7A6A5A] bg-[#FFFBF2] rounded-xl w-full border-[' type="text" placeholder=" enter your full name" id="name"></input>
         </div>
 
         <div>
-        <label  className='font-[Inter] text-[#7A6A5A] font-medium' htmlFor="password" value="password">Password</label>
-        <input className='bg-[#ffffff] rounded-xl w-full' type="password" placeholder="" id="password"/>
+        <label className='font-sans text-[#7A6A5A] font-medium' htmlFor="email" value="email" required>Email</label>
+        <input className='placeholder:text-[#e6cbb2] placeholder:font-extralight placeholder:font-sans border border-[#EFE7D6] focus:outline-none bg-[#FFFBF2] rounded-xl p-[50] w-full text-[#7A6A5A]' type="email" placeholder=" enter your email" id="email"/>
         </div>
 
         <div>
-        <label className='font-[Inter] text-[#7A6A5A] font-medium'>Confirm Password</label>
-        <input className='bg-[#ffffff] rounded-xl w-full' type="password"/>
+        <label  className='font-sans text-[#7A6A5A] font-medium' htmlFor="password" value="password">Password</label>
+        <input className='placeholder:text-[#e6cbb2] placeholder:font-extralight placeholder:font-sans border border-[#EFE7D6] focus:outline-none bg-[#FFFBF2] rounded-xl w-full text-[#7A6A5A]' type="password" placeholder="" id="password"/>
         </div>
 
         <div>
-          <input type="checkbox"/>
-             <label className='font-[Inter] text-[#7A6A5A] text-[14px] font-medium'> I agree to the Terms of Service and Privacy Policy</label>
+        <label className='font-sans text-[#7A6A5A] font-medium'>Confirm Password</label>
+        <input className='placeholder:text-[#e6cbb2] placeholder:font-extralight placeholder:font-sans border border-[#EFE7D6] focus:outline-none bg-[#FFFBF2] rounded-xl w-full text-[#7A6A5A]' type="password"/>
         </div>
 
-        <button className='bg-[#8D7B68] flex justify-center text-[#FFFFFF] font-[Inter] rounded-full font-medium text-center w-full h-[54]'>
-          <Star strokeWidth={2.5} size={9}></Star>Create Account</button>
-       <div className='space-y-4'>
-        <p className='text-[#7A6A5A] font-medium text-center'>Already have an account ? Sign in</p> 
+        <div>
+          <input type="checkbox" required/>
+             <label className='font-sans text-[#7A6A5A] text-[14px] font-normal'> I agree to the Terms of Service and Privacy Policy</label>
         </div>
-      </form>
 
-    <div className='flex justify-center space-y-4'>
-      <Sparkle size={10} strokeWidth={2.5} className='text-[#5C544B]'/>
-      <p className='text-[#5C544B] text-[14px]'>Back to Home</p>
-      <Sparkle size={10} strokeWidth={2.5} className='text-[#5C544B]'/>
+       <div>
+        <button className='bg-[#8D7B68] text-[#FFFFFF] font-[Inter] rounded-full font-medium text-center w-full py-2 px-0.5 flex justify-center items-center gap-1 shadow-2xl'>
+          <Star strokeWidth={2.5} size={9}></Star><span className='font-medium'>Create Account</span></button>
+      </div>
+
+       <div className='mt-1.5 mb-0.5'>
+        <p className='text-[#7A6A5A] font-medium text-center'>Already have an account ? <span className='underline'>Sign in</span></p> 
+      </div>
+    </form>
+
+    <div className='flex items-center mb-3.5 gap-0.5 mt-3.5'>
+      <House size={13} className='text-[#5C544B]'/>
+      <p className='text-[#5C544B] text-[14px] hover:underline'>Back to Home</p>
+      
      </div> 
     </div>
   )
