a62d8f280c2d05a6f60a9888cbb7dadf8befad8a
Mimi986 <chelouah.mounia@gmail.com>
2026-04-25 22:56:03 +0100

    feat(commencement des booksCards dans le dashboard)

--- client/src/Components/dashboard/BookCard.jsx
+++ client/src/Components/dashboard/BookCard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const BookCard = () => {
   return (
-    <div>BookCard</div>
+    <div className='bg-[#FAF6F0] transition-all duration-300 hover:-translate-y-3 hover:shadow-2xl rounded-2xl'>
+
+    </div>
   )
 }
 
